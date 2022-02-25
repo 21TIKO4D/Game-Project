8fa1471c52dd570e665e3102cf8e6278f0c9841d
Allu101 <a.lahtinen101@gmail.com>
2022-02-25 20:41:30 +0200

    Added Unity project

---
+++ Assets/Codes/InputProcessor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PeliExample
+{
+    public class InputProcessor : MonoBehaviour
+    {
+        [SerializeField]
+        private InputActionAsset inputControls;
+
+        [SerializeField]
+        private string actionMapName;
+
+        private InputAction[] inputActions;
+
+        private void Awake()
+        {
+            InputActionMap actionMap = inputControls.FindActionMap(actionMapName);
+            inputActions = actionMap.actions.ToArray();
+
+        }
+
+        private void OnEnable()
+        {
+            foreach (var action in inputActions)
+            {
+                action.Enable();
+                action.performed += ProcessInput;
+                action.canceled += ProcessInput;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var action in inputActions)
+            {
+                action.Enable();
+                action.performed -= ProcessInput;
+                action.canceled -= ProcessInput;
+            }
+        }
+
+        private void ProcessInput(InputAction.CallbackContext callbackContext) {
+            SendMessage($"On{callbackContext.action.name}", callbackContext, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
