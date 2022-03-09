49271ad7ebdf39f707977db03b5ee7bd3a1f784d
Allu101 <a.lahtinen101@gmail.com>
2022-03-09 16:35:45 +0200

    Increased map size, added cow texture to animal prefab and added 4 animals to game

---
+++ Assets/Codes/Animal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace PeliExample
+{
+    public class Animal : MonoBehaviour
+    {
+        public SnakeManager snakeManager;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player")) {
+                snakeManager.Grow();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
