7ad8dd44b81702a966dbb576614cc2e64820c441
Allu101 <a.lahtinen101@gmail.com>
2022-03-18 17:02:29 +0200

    Added simple pause menu

---
+++ Assets/Codes/PauseMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private static bool GameIsPaused = false;
+    [SerializeField]
+    private GameObject PauseMenuUI;
+
+    public void Start()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+    
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
