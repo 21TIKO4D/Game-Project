59866685a1b9f294fc77f76ba3809810b9254cca
Allu101 <a.lahtinen101@gmail.com>
2022-04-24 17:40:29 +0300

    Added sfx to train collision, made minor improvements to animal sounds and minor project clean up

--- Assets/Codes/TrainCollision.cs
+++ Assets/Codes/TrainCollision.cs
@@ -1,21 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TrainCollision : MonoBehaviour
 {
+    private static float lastCollisionTime = 0;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Obstacle"))
+        if (other.gameObject.CompareTag("Obstacle") && lastCollisionTime > 0.4f)
         {
+            lastCollisionTime = 0;
             this.GetComponentInParent<TrainManager>().OnTrainCollision();
             
-            ParticleSystem effect = other.gameObject.transform.parent.GetChild(0).GetComponent<ParticleSystem>();
+            ParticleSystem effect = null;
+            AudioSource audio = null;
+            foreach (Transform child in other.gameObject.transform.parent)
+            {
+                if (child.name == "CollisionEffect")
+                {
+                    effect = child.GetComponent<ParticleSystem>();
+                    audio = child.GetComponent<AudioSource>();
+                    break;
+                }
+            }
             if (effect != null)
             {
                 effect.gameObject.transform.position = this.gameObject.transform.position;
+                audio.Play();
                 effect.Play();
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        lastCollisionTime += Time.fixedDeltaTime;
     }
 }
