773f03054e39dda5ffcf323529a80e3b2764b609
Allu101 <a.lahtinen101@gmail.com>
2022-04-27 11:56:20 +0300

    Added small turn 'feature' to animals

--- Assets/Codes/Animal.cs
+++ Assets/Codes/Animal.cs
@@ -8,6 +8,44 @@ public class Animal : MonoBehaviour
         set;
     }
 
+    private enum State
+    {
+        IDLE,
+        TURN
+    }
+    private State state;
+    private float timer = 3;
+
+    private float multiplier = 1;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            if (state == State.IDLE)
+            {
+                state = State.TURN;
+                timer = 1f;
+                multiplier = Random.Range(0, 4) < 2 ? -1 : 1;
+            } else
+            {
+                state = State.IDLE;
+                timer = 7 + Random.Range(-1.5f, 1.5f);
+            }
+        }
+        switch (state)
+        {
+            case State.TURN:
+                Vector3 rotation = transform.eulerAngles;
+                float value = timer > 0.2 ? 0.23f : 0.15f;
+                rotation.z += multiplier * value;
+                transform.rotation = Quaternion.Euler(rotation);
+                break;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player")) {
